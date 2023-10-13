abc4d251d3ea0e67ceed221aa9cfdc0c2668f346
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-10-13 05:04:47 -0400

    Initial commit

--- MyCustomElement/MyCustomElement.ts
+++ MyCustomElement/MyCustomElement.ts
@@ -32,14 +32,14 @@ export class MyCustomElement extends HTMLElement{
         <div itemscope>
             <div  id=someNumPropVal></div>
             <div  id=someStringPropVal></div>
-            <!-- <h3>Example 1a</h3>
+            <h3>Example 1a</h3>
             <input type=number disabled be-elevating='to some num prop.'>
             <h3>Example 1b</h3>
             <input data-test='this text should appear on input.' disabled 
                     be-elevating='of dataset:test to some string prop.'>
             <h3>Example 1c</h3>
             <input data-test='this text should appear on input.' disabled 
-                    be-elevating='of dataset:test to some string prop on change.'> -->
+                    be-elevating='of dataset:test to some string prop on change.'>
             <h3>Example 2a</h3>
             <my-peer-element -my-string-prop></my-peer-element>
             ...
