acf5604cffd7fd63279ba2bf4af094629ad1d39f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-26 09:12:44 -0500

    0.0.5

--- tests/Example1a.spec.mjs
+++ tests/Example1a.spec.mjs
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-test('test1', async ({ page }) => {
-    await page.goto('./tests/AllExamples.html');
+test('Example1a', async ({ page }) => {
+    await page.goto('./tests/Example1a.html');
     // wait for 1 second
     await page.waitForTimeout(1000);
     const editor = page.locator('#target');
